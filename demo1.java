4655f8713c2263035c52e5fc0905e191ba8553a4
test <satyam4cs@gmail.com>
2019-07-30 11:53:17 +0530

    2nd commit

--- demo1.java
+++ demo1.java
@@ -1,6 +1,26 @@
 public class demo1 {
 
+    public int add(int x, int y) {
+        System.out.println("Add function");
+        return add(x, y);
+    }
+
+    int subtract(int x, int y) {
+        System.out.println("Subract function");
+        return x-y;
+    }
+
+    int multiply(int x, int y) {
+        System.out.println("Multiply function");
+        return x*y;
+    }
+
+    void divide(int x, int y) {
+        System.out.println("Divide function");
+        //pass x/y
+    }
     public static void main(String[] args) {
-        System.out.println("Hello DB!");
+
+
     }
 }
